91588f38d877d824be7b086371dff90662040573
AF1nd <101056957+AF1nd@users.noreply.github.com>
2024-12-13 00:52:16 +0700

    print elapsed time

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,14 @@
+#include <time.h> 
+#include <chrono>
+
 #include "include/vm.h"
 #include "compiler/include/lexer.h"
 
 int main()
 {
     FemiraVirtualMachine vm;
+
+    auto start = chrono::high_resolution_clock::now();
 
     vm.runf_bytecode({
         Instruction(Opcode(OP_PUSHV), new Integer(0x1)), // push address
@@ -15,10 +20,18 @@ int main()
             Instruction(Opcode(OP_RETURN)),
         }, 0)), // write fn to this address
         Instruction(Opcode(OP_READ_DATA), new Integer(0x1)), // get fn by address
+
+        Instruction(Opcode(OP_PUSHV), new Double(1.5)),
+        Instruction(Opcode(OP_WAIT)),
         
         Instruction(Opcode(OP_CALL)), // call fn
         Instruction(Opcode(OP_PRINT)) // print result
     }, true);
+
+    auto end = chrono::high_resolution_clock::now();
+    chrono::duration<double> elapsed_seconds = end - start;
+
+    cout << "Elapsed time: " << elapsed_seconds.count() << " seconds\n";
 
     return 0;
 }
